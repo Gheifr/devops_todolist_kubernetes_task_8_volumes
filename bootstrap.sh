1d53a8b214362d699d5cf5c46e659703c15fbc50
Gheifr <48731758+Gheifr@users.noreply.github.com>
2025-05-08 23:12:34 +0300

    Solution 3

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ set -e
 
 kubectl apply -f .infrastructure/namespace.yml
 
-kubectl apply -f .infrastructure/configMap.yml
-kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/app-config.yml
+kubectl apply -f .infrastructure/app-secret.yml
 
 kubectl apply -f .infrastructure/pv.yml
 kubectl apply -f .infrastructure/pvc.yml
